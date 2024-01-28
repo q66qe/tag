026e2e6c2d6ab82cb93828d9e06ad03f6b0cbc17
Luffy <142293678+q66qe@users.noreply.github.com>
2024-01-28 20:23:37 +0300

    Update ELHYBA.py

--- ELHYBA.py
+++ ELHYBA.py
@@ -381,7 +381,86 @@ async def on_Callback(c,m):
 
 
 #############################################################################
+# Dev RAD - @R_AFX  Telegram Channls : @radfx2 
 
+BOT_MESSAGE = {
+    'JOIN_CHANLL':
+                u'عذرن عزيزي عليك الاشترك بي قناة البوت اولان لي استخدام البوت 🧩💬.'
+                u'\n\n Channl : @{} 💭🔰.'
+                u'\n\n  👇 قم بي الاشترك من ثم اضغط علا زر تحقق 🔱🔗 .'
+                u'',
+    'DONE_JOIN_CHANNL':ers 
+
+                    u'شكرأ لك على الاشتراك في قنات البوت 〽️🧩.'
+                    u'\n\n الان فم بي ارسال ( /start ) لي تشغيل البوت ♻️🔱.'
+                    u'\n\n اتمنة لك تجربا ممتعة ✅🤍'
+                    u'\n معا تحيات 𝗗𝗘𝗩 RAD - @R_AFX 💬👨‍💻'
+                    u''
+}
+
+def CHECK_JOIN_KEYBOARD(Channl: str):
+    return types.InlineKeyboardMarkup([
+        [
+            types.InlineKeyboardButton(text='قناة البوت 〽️💭.', url=f't.me/{Channl}'),
+            types.InlineKeyboardButton(text='تحقق ♻️.', callback_data='checkjoin')
+        ]
+    ])
+def REDRESH_LANSHER(text: str):
+    return types.InlineKeyboardMarkup([
+        [
+            types.InlineKeyboardButton(text=text, callback_data='NOT')
+        ]
+    ])
+    
+## api chack member join from channls
+async def CHECK_USER_JOIN(api_key, channls_join: list, user_id : int):
+    c ,r = None ,False
+    statues = ['administrator','creator','member','restricted']
+    for channl in channls_join:
+        url =f"https://api.telegram.org/bot{api_key}/getChatMember?chat_id=@{channl}&user_id={str(user_id)}"
+        respons = get(url)
+        JSObj = json.loads(respons.text) 
+        user_state = JSObj['result']['status']
+        if user_state in statues:
+            r = True 
+        else : 
+            r = False
+            c = channl
+            return r,c
+    return r,c
+
+BOT_CHANNL = ['TeamRecorder']
+app = Client(
+    'rad',
+    bot_token=Config.TG_BOT_TOKEN, 
+    api_id=22119881 , # userbot api id  
+    api_hash='95f5f60466a696e33a34f297c734d048' # userbot api hash
+)
+
+@app.on_message(filters.regex('^/start$') & filters.private)
+async def START_BOT(_, message: types.Message):
+    chat_id, message_id, user_id = message.chat.id, message.id, message.from_user.id
+    join_, channl = await CHECK_USER_JOIN(API_KEY,BOT_CHANNL, user_id)
+    if not join_:
+        await app.send_message(chat_id=chat_id, text=BOT_MESSAGE['JOIN_CHANLL'].format(channl), reply_markup=CHECK_JOIN_KEYBOARD(channl))
+        return 
+    await app.send_message(chat_id, 'Welcome to bot .')
+
+
+@app.on_callback_query(filters.regex('^checkjoin$'))
+async def CHAECK_JOIN(_, query: types.CallbackQuery):
+    await app.edit_message_text(text='انتضر جاري التحقق من الاشتراك ⚙️.', reply_markup=REDRESH_LANSHER('تحقق من الاشتراك♻️⚙️.'), chat_id=query.message.chat.id, message_id=query.message.id)
+    await asyncio.sleep(0.3)
+    join_, channl = await CHECK_USER_JOIN(API_KEY, BOT_CHANNL, query.from_user.id)
+    if not join_:
+        await app.edit_message_text(text=BOT_MESSAGE['JOIN_CHANLL'].format(channl), reply_markup=CHECK_JOIN_KEYBOARD(channl) ,chat_id= query.message.chat.id, message_id=query.message.id)    
+        await app.answer_callback_query(query.id, 'تأكد من اشتراك في القناة و اعد المحاولا ✅〽️.', show_alert=True)  
+        return
+    await app.edit_message_text(text=BOT_MESSAGE['DONE_JOIN_CHANNL'], chat_id= query.message.chat.id, message_id=query.message.id)
+
+
+
+asyncio.run(app.run())
 @app.on_message(filters.command("start") & filters.private)
 async def start_msg(app, message):
       reply_markup = ReplyKeyboardMarkup(
